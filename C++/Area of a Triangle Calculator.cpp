71542b28de10b3e283874a83abc2df83c3d9adc7
isovanillin <74217369+isovanillin@users.noreply.github.com>
2024-10-22 00:58:06 +0000

    Create Area of a Triangle Calculator.cpp

---
+++ C++/Area of a Triangle Calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main()
+{
+   double height{}, base{}, yeah{0.5};
+   std::cout << "enter the base and then the height fr :fire:\n";
+   std::cin >> base >> height;
+
+
+   std::cout << "the area of yuor triangles.... is " << ((yeah)*(height)*(base));
+
+
+}
+
+// Enter base, then height of your triangle.
+// Misspellings on purpose!!!!!
