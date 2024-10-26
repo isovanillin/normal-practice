76ff5181a5b5381f6ba2b9b7b7c9fe9bca650c1b
isovanillin <74217369+isovanillin@users.noreply.github.com>
2024-10-26 14:39:28 -0400

    Create Even Numbers up to 10.cpp

---
+++ C++/Even Numbers up to 10.cpp
@@ -0,0 +1,39 @@
+/* #include <iostream>
+
+int main(){
+
+int i{};
+
+for (i = 0; i < 10; ++i)
+{
+	if (i % 2 == 0)
+	{
+		std::cout << i;
+	}
+	
+	else
+	{
+		std::cout << " ";
+	}
+	
+}}
+*/
+
+// want to try this again in the near future
+
+// second attempt
+#include <iostream>
+
+int main(){
+
+int i{};
+
+for (i = 0; i < 10; ++i)
+{
+	if (i % 2 == 0)
+	{
+		std::cout << i << " ";
+	}
+	
+	
+}}
