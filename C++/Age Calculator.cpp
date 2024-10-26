0688dc3a0c34be8a8fb699e3ac62a460be151331
isovanillin <74217369+isovanillin@users.noreply.github.com>
2024-10-26 14:27:18 -0400

    Create Age Calculator.cpp

---
+++ C++/Age Calculator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+int birth{}, current{};
+std::cin >> birth >> current;
+
+std::cout << "You are currently " << (current - birth) << " years old.";
+
+}
+// yeah i know sometimes it doesn't work like this and months and stuff ill make a new one later probably
